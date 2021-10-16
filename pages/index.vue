6c3252bf71f2540b3046f1656071349ec98d1305
David Bouhaben <davidbouhaben@yahoo.co.id>
2021-10-16 11:51:44 +0200

    first page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
 <template>
-  <div id="__nuxt">
-    <div id="__layout"></div>
-  </div>
+  <body data-aos-easing="ease" data-aos-duration="1250" data-aos-delay="100">
+    <div id="__nuxt">
+      <div id="__layout">
+        <div class="template-shoppingauto"><Aos /></div>
+      </div>
+    </div>
+  </body>
 </template>
 
 <script>
