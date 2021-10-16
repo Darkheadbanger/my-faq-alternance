c9f06fe13828f43c4e02bcdd48e5f4c492084a79
David Bouhaben <davidbouhaben@yahoo.co.id>
2021-10-16 19:12:57 +0200

    Code organization

---
+++ components/footer.vue
@@ -0,0 +1,74 @@
+<template>
+  <!-- footer qui englobe tout -->
+  <footer>
+    <!-- Pour organiser -->
+    <div>
+      <!-- container footer -->
+      <div>
+        <!-- logo shopping auto -->
+        <div>
+          <a href="">
+            <img src="" alt="" />
+          </a>
+        </div>
+        <!-- container navigation -->
+        <div>
+          <!-- Navigation -->
+          <p></p>
+          <!-- list de nav -->
+          <nav>
+            <!-- 10X -->
+            <div>
+              <a href=""></a>
+            </div>
+          </nav>
+        </div>
+        <!-- container rs -->
+        <div>
+            <p>
+                <!-- reseaux sociaux -->
+                <nav>
+                    <!-- a 3X -->
+                    <a href="">
+                        <svg>
+                            <path>
+                                
+                            </path>
+                        </svg>
+                    </a>
+                </nav>
+            </p>
+        </div>
+      </div>
+    </div>
+    <!-- Le crezdit dernier -->
+    <div>
+      <div>
+        <p>
+          <span></span>
+          <a href="">
+            <!-- Logo eveho -->
+            <svg>
+              <path></path>
+            </svg>
+          </a>
+        </p>
+        <!-- Mention legale -->
+        <p>
+          <!-- shopping auto.fr -->
+          <a href="">
+            <!-- mention legale -->
+          </a>
+          <span>
+            <!-- et -->
+            <span>cgs</span>
+          </span>
+        </p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
