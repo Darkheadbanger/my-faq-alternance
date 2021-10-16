4640953695c88e9e95fd047248e2f49f6b463fe9
David Bouhaben <davidbouhaben@yahoo.co.id>
2021-10-16 15:19:40 +0200

    scss worked

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <body data-aos-easing="ease" data-aos-duration="1250" data-aos-delay="100">
+  <body
+    id="body"
+    data-aos-easing="ease"
+    data-aos-duration="1250"
+    data-aos-delay="100"
+  >
     <div id="__nuxt">
       <div id="__layout">
         <div class="template-shoppingauto"><Aos /></div>
@@ -11,3 +16,17 @@
 <script>
 export default {}
 </script>
+<style lang="scss">
+$border_logo: #e2e8f0;
+$background_color: #f00;
+
+* {
+  box-sizing: border-box;
+  border: 0 solid $border_logo;
+}
+#body {
+  overflow-x: hidden;
+  margin: 0;
+  background-color: $background_color;
+}
+</style>
