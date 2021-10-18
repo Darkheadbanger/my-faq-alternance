187b4cff8fdc8d55e1cae2fa258062d212927517
David Bouhaben <davidbouhaben@yahoo.co.id>
2021-10-18 11:04:14 +0200

    nav menu

--- components/Nav.vue
+++ components/Nav.vue
@@ -27,22 +27,102 @@
           <li class="container-dropdown">
             <a href="/voitures occasion">
               <img
-                src="h-5 md:3 inline mr-2 my-auto"
-                alt="~/assets/images/menu/voiture.png"
+                class="h-5 md:3 inline mr-2 my-auto"
+                src="~/assets/images/menu/car.svg"
+                alt="Voitures"
               />
+              Nos voitures
             </a>
           </li>
           <!-- Pour ma liste -->
           <!-- Ma liste -->
           <li>
             <div class="border border-primary py-1 px-2 rounded-full">
-              <a href="~/assets/images/menu/heart.png">
+              <a href="/mon-compte/wishlist">
                 <!-- Symbol coeur -->
-                <svg>
-                  <path></path>
+                <svg
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="far"
+                  data-icon="heart"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  class="
+                    text-sm
+                    my-auto
+                    md:mt-1
+                    text-primary
+                    svg-inline--fa
+                    fa-heart fa-w-16
+                  "
+                >
+                  <path
+                    fill="currentColor"
+                    d="M458.4 64.3C400.6 15.7 311.3 23 256 79.3 200.7 23 111.4 15.6 53.6 64.3-21.6 127.6-10.6 230.8 43 285.5l175.4 178.7c10 10.2 23.4 15.9 37.6 15.9 14.3 0 27.6-5.6 37.6-15.8L469 285.6c53.5-54.7 64.7-157.9-10.6-221.3zm-23.6 187.5L259.4 430.5c-2.4 2.4-4.4 2.4-6.8 0L77.2 251.8c-36.5-37.2-43.9-107.6 7.3-150.7 38.9-32.7 98.9-27.8 136.5 10.5l35 35.7 35-35.7c37.8-38.5 97.8-43.2 136.5-10.6 51.1 43.1 43.5 113.9 7.3 150.8z"
+                    class=""
+                  ></path>
                 </svg>
-                <span class="my-auto text-xs mr-1">(0)</span>
-                <span>Ma liste</span>
+                <span class="my-auto text-xs mr-1"> (0) </span>
+                <span> Ma liste </span>
+              </a>
+            </div>
+          </li>
+
+          <li>
+            <div class="border border-primary py-1 px-2 rounded-full">
+              <a href="/garanties-shoppingauto">
+                <!-- Symbol coeur -->
+                <img
+                  class="h-5 md:3 inline mr-2 my-auto"
+                  src="~/assets/images/menu/Garanties.svg"
+                  alt="Garanties"
+                />
+                Nos garanties
+              </a>
+            </div>
+          </li>
+
+          <li>
+            <div class="border border-primary py-1 px-2 rounded-full">
+              <a href="/qui-sommes-nous">
+                <!-- Symbol coeur -->
+                <img
+                  class="h-5 md:3 inline mr-2 my-auto"
+                  src="~/assets/images/menu/list.svg"
+                  alt="Qui sommes-nous ?"
+                />
+                Qui sommes-nous ?
+              </a>
+            </div>
+          </li>
+
+          <li>
+            <div class="border border-primary py-1 px-2 rounded-full">
+              <a
+                class="nuxt-link-exact-active nuxt-link-active"
+                href="/garanties-shoppingauto"
+              >
+                <!-- Symbol coeur -->
+                <img
+                  class="h-5 md:3 inline mr-2 my-auto"
+                  src="~/assets/images/menu/chat.svg"
+                  alt="FAQ"
+                />
+                FAQ
+              </a>
+            </div>
+          </li>
+
+          <li>
+            <div class="border border-primary py-1 px-2 rounded-full">
+              <a href="/garanties-shoppingauto">
+                <img
+                  class="h-5 md:3 inline mr-2 my-auto"
+                  src="~/assets/images/menu/person.svg"
+                  alt="Garanties"
+                />
+                Me connecter
               </a>
             </div>
           </li>
