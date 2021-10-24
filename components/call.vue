48b557c493c8ea0ce288bdbdb82ff5af1e929783
David Bouhaben <davidbouhaben@yahoo.co.id>
2021-10-24 20:05:00 +0200

    call button en cours

--- components/call.vue
+++ components/call.vue
@@ -12,9 +12,19 @@
             href="#appelez-nous"
             target=""
           >
-            <span>Appelez-nous</span>
+            <span class="container-text">Appelez-nous</span>
             <div class="container-i">
-              <font-awesome-icon :icon="['fas', 'phone']" />
+              <font-awesome-icon
+                class="
+                  text-white text-xl
+                  m-auto
+                  svg-inline--fa
+                  fa-phone fa-w-16
+                "
+                aria-hidden="true"
+                focusable="false"
+                :icon="['fas', 'phone']"
+              />
             </div>
           </button>
         </li>
@@ -28,13 +38,43 @@ export default {}
 </script>
 
 <style lang="scss" scoped>
-.container-i {
-  display: flex;
-  opacity: 1;
-  background-color: #ee285b;
-  background-color: rgba(238, 40, 91, var(--bg-opacity));
-  height: 50px;
-  width: 50px;
-  z-index: 4;
+div {
+  .container-sidebar {
+    transform: translateY(-50%);
+    top: 50%;
+    .container-text {
+      background-color: $call-button_color;
+      margin-top: auto;
+      margin-bottom: auto;
+      position: absolute;
+      font-size: 1rem;
+      text-transform: uppercase;
+      display: flex;
+      transition: all 0.4s;
+      width: 250px;
+      height: 50px;
+      transform: translate(250px);
+      .container-i {
+        display: flex;
+        opacity: 1;
+        background-color: $call-button_color;
+        height: 50px;
+        width: 50px;
+        z-index: 4;
+      }
+    }
+  }
+  ul {
+    list-style: none;
+    li {
+      margin: 0;
+      padding: 0;
+    }
+  }
+  button {
+    background-color: transparent;
+    background-image: none;
+    text-transform: none;
+  }
 }
 </style>
